5b9321551eae1c032dd5d88039fce981380242e9
Quang-IT <22130229@st.hcmuaf.edu.vn>
2026-08-24 10:47:24 +0700

    Clean up repository: Remove redundant frontend and build artifacts

--- nlufood-frontend/android/app/src/main/java/com/nlufood/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.nlufood.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- nlufood-frontend/src/config.js
+++
@@ -1,2 +0,0 @@
-// Local IP Address of host machine on Wi-Fi network
-export const API_BASE_URL = 'http://192.168.221.58:8080/api';
